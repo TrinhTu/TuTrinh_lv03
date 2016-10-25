ffa27a3f1e5d22f9ac66bf86b1678504134e8c14
Lê Tú Trinh <letutrinh.ktmm@gmail.com>
2016-10-25 21:36:36 +0700

    Create lv03_While01.c

---
+++ lv03_While01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,r;
+    printf("Nhap 2 so a b:");
+    scanf("%d%d",&a,&b);
+    while(r!=0)
+    {
+        r=a%b;
+        a=b;
+        b=r;
+    }
+    printf("UCLN=%d",a);
+}
